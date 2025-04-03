512fbf595da8d807d35c1891ab4d587b09d974e8
Muhammad Anas <anasabid607@gmail.com>
2025-04-03 20:01:45 +0200

    Estructura_Y contador de kills

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,3 +1,18 @@
 //Moviment Player1
 const player1 = document.getElementById("player1") ;
 let position = 0;
+
+
+// Contador eliminaciones
+let killsp1 = 0;
+let killsp2 = 0;
+
+function sumarKillP1(){
+    killsp2 += 1;
+    document.querySelector("#killsPlayer1").innerHTML = killsp1;
+}
+
+function sumarKillP2() {
+    killsp1 += 1;
+    document.querySelector("#killsPlayer2").innerHTML = killsp2;
+}
